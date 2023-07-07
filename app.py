985d9474dbe8e04b9df0e0edf17593c59316d60b
unknown <kselvaggi13@gmail.com>
2023-07-06 20:41:50 -0400

    looks a bit better now

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from scraping import get_videos
+import os
 
 app = Flask(__name__)
 
@@ -14,16 +15,8 @@ def home():
             processed_video_data[teams] = processed_links
         return render_template('index.html', video_data=processed_video_data)
     else:
-        video_data = {"use Refresh button to view VODS. Note that this will open a chrome window, don't look at it to avoid spoilers": "data"}
-        processed_video_data = {}
-        for teams, links in video_data.items():
-            processed_links = [(i+1, link) for i, link in enumerate(links)]
-            processed_video_data[teams] = processed_links
-        return render_template('index.html', video_data=processed_video_data)
-
-
-@app.route('/refresh', methods=['GET', 'POST'])
-def refresh():
-    if request.method == 'POST':
-        video_data = get_videos()
-        return render_template('index.html', video_data=video_data)
+        return render_template('index.html', video_data=None)
+    
+@app.route('/favicon.ico', methods=['GET'])
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
