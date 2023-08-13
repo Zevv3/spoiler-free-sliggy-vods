db340cb234b03dceeb7b97a26b9850765a3dc9ff
unknown <kselvaggi13@gmail.com>
2023-08-13 12:29:50 -0400

    restructured for hosting

---
+++ vods/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, send_from_directory
+from scraping import get_videos
+import os
+import time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        video_data = {}
+        video_data = get_videos()
+        processed_video_data = {}
+        for teams, links in video_data.items():
+            processed_links = [(i+1, link) for i, link in enumerate(links)]
+            processed_video_data[teams] = processed_links
+        return render_template('index.html', video_data=processed_video_data)
+    else:
+        return render_template('index.html', video_data=None)
+    
+MAX_RETRIES = 3
+RETRY_DELAY = 1
+
+@app.route('/favicon.ico')
+def favicon():
+    retries = 0
+    while retries < MAX_RETRIES:
+        try:
+            return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+        except FileNotFoundError:
+            retries += 1
+            print('retry')
+            time.sleep(RETRY_DELAY)
+    return ''
---
+++ vods/scraping.py
@@ -0,0 +1,82 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+import time
+from collections import deque
+
+def get_videos():
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
+    driver = webdriver.Firefox(options=options)
+    driver.get('https://www.youtube.com/@Sliggytv/videos')
+
+    WAIT_IN_SECONDS = 2
+    SCROLL_TIMES = 3 
+    scroll_count = 0
+
+    last_height = driver.execute_script("return document.documentElement.scrollHeight")
+
+    while scroll_count < SCROLL_TIMES:
+        # Scroll to the bottom of the page
+        driver.execute_script("window.scrollTo(0, arguments[0]);", last_height)
+        # Wait for new videos to show up
+        time.sleep(WAIT_IN_SECONDS)
+
+        # Calculate the new document height and compare it with the last height
+        new_height = driver.execute_script("return document.documentElement.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+        scroll_count += 1
+        
+    # Continue with scraping the loaded videos
+
+
+    titles = driver.find_elements(By.ID, 'video-title')
+    links = driver.find_elements(By.ID, 'video-title-link')
+    # driver.find_element(By.CLASS_NAME)
+
+    videos = {title.text: link.get_attribute('href') for title, link in zip(titles, links)}
+
+    # titles = [title.text for title in titles]
+    # teams = [title.split('Map')[0] for title in titles]
+    # links = [link.get_attribute('href') for link in links]
+
+    series = {}
+    for title, link in videos.items():
+        if 'final' in title.lower() or 'finals' in title.lower():
+            best_of = 5
+        else:
+            best_of = 3
+        split_title = title.lower().split(' ')
+        teams = split_title[0].upper() + ' ' + split_title[1] + ' ' + split_title[2].upper()
+        if best_of == 5:
+            if teams + ' finals' not in series:
+                default_value = 'https://www.youtube.com/'
+                maps = deque([default_value] * best_of)
+                series[teams + ' finals'] = maps
+                maps.appendleft(link)
+                maps.pop()
+            else:
+                maps = series[teams + ' finals']
+                maps.appendleft(link)
+                maps.pop()
+        if best_of == 3:
+            if teams not in series:
+                default_value = 'https://www.youtube.com/'
+                maps = deque([default_value] * best_of)
+                series[teams] = maps
+                maps.appendleft(link)
+                maps.pop()
+            else:
+                maps = series[teams]
+                maps.appendleft(link)
+                maps.pop()
+        
+        
+    return series
